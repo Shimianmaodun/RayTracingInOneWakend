ec75de4abf97b4facdf7e27f2f48c509fd5dcb77
DESKTOP-3OI8D7P\嘻嘻哈哈 <2960587137@qq.com>
2025-03-23 17:37:32 +0800

    创建Ray 简单相机和背景

--- RayTracingIInOneWakend/RayTracingIInOneWakend/vec3.h
+++ RayTracingIInOneWakend/RayTracingIInOneWakend/vec3.h
@@ -77,7 +77,7 @@ inline vec3 dross(const vec3& u, const vec3& v) {
 				u.e[0] * v.e[1] - u.e[1] * v.e[0]
 	);
 }
-inline vec3 uint_vector(const vec3& v) {
+inline vec3 unit_vector(const vec3& v) {
 	return v / v.length();
 }
 
