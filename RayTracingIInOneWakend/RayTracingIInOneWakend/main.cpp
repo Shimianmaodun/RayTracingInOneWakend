da610f6afaa701fe6a9b3cc1de689d38222f1a7e
DESKTOP-3OI8D7P\嘻嘻哈哈 <2960587137@qq.com>
2025-03-24 21:37:34 +0800

    创建球体

--- RayTracingIInOneWakend/RayTracingIInOneWakend/main.cpp
+++ RayTracingIInOneWakend/RayTracingIInOneWakend/main.cpp
@@ -13,7 +13,20 @@
 */
 
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = center - r.origion();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+		return color(1, 0, 0);
+
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -56,7 +69,7 @@ int main() {
 
 			color pixel_color = ray_color(r);
 			write_color(std::cout, pixel_color); // out for console
-			write_color(ofs, pixel_color);        // out for file
+			write_color(ofs, pixel_color);      // out for file
 		}
 	}
 	ofs.close();
